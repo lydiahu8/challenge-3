874cd90cc2c5680502091d0501013783649fdeef
lydiahu8 <lydiahu8@gmail.com>
2020-08-16 18:02:49 -0700

    Add Challenge 3

--- src/challenge-3.ts
+++ src/challenge-3.ts
@@ -1,9 +1,43 @@
-type ValidJSON = ValidJSONObject // | ... | ... | ...
+type ValidJSON = ValidJSONObject;
+type isPrimitive = string | number | boolean;
+type isReference = object | isPrimitive[] | isNullAndUndefined;
+type isNullAndUndefined = null | undefined;
 
 interface ValidJSONObject {
-  // ...
+  input: isPrimitive | isReference | isNullAndUndefined;
+  [key: string]: isPrimitive | isReference | isNullAndUndefined;
 }
 
-export const stringify = (input: any): string => (
-  '""'
-);
+export const stringify = (input: ValidJSONObject["input"]): string => {
+  if (
+    typeof input === "number" ||
+    typeof input === "boolean" ||
+    input === null
+  ) {
+    return `${input}`;
+  }
+
+  if (typeof input === "string") {
+    const inputStrArr: Array<string> = input.split(/\n/);
+
+    if (inputStrArr.length > 1) {
+      return `"${inputStrArr.join(/\n/)}"`.replace(/\//g, "");
+    } else {
+      return `"${input}"`;
+    }
+  }
+
+  let results: string[] = [];
+  if (Array.isArray(input)) {
+    input.forEach((element) => results.push(stringify(element)));
+    return `[${results.join(",")}]`;
+  } else {
+    for (let key in input) {
+      const value: ValidJSONObject["input"] = input[key];
+      if (value !== undefined || typeof value !== "function") {
+        results.push(`${stringify(key)}:${stringify(value)}`);
+      }
+    }
+    return `{${results.join(",")}}`;
+  }
+};
